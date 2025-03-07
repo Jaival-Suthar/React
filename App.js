58eca28bd806700205a1c3480da1811641e41fff
Jaival <jaivalsuthar21@gmail.com>
2025-03-07 10:42:44 +0530

    feat: import React & ReactDOM, create nested structure with createElement

--- App.js
+++ App.js
@@ -1,3 +1,17 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World from React");
+// const heading = React.createElement("h1",{id:"heading"},"Hello World from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I'm a H1 tag"),
+    React.createElement("h2",{},"I'm a H2 tag")
+   ]),
+    React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I'm a H1 tag"),
+    React.createElement("h2",{},"I'm a H2 tag")
+   ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React Learning Journey
+
+# What Parcel is doing ?
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization 
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling ( support older browers )
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree shaking ( remove unused code )
+- Different Dev and Prod Bundles
