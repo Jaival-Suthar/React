c67df431c5b876dfe721f5599c7a387d3300eec7
Jaival <jaivalsuthar21@gmail.com>
2025-03-07 23:00:08 +0530

    feat: add npm scripts (start & build), basic React component, and nested component

--- App.js
+++ App.js
@@ -3,15 +3,19 @@
  import React from "react";
  import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I'm a H1 tag"),
-    React.createElement("h2",{},"I'm a H2 tag")
-   ]),
-    React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I'm a H1 tag"),
-    React.createElement("h2",{},"I'm a H2 tag")
-   ]),
-]);
+// JSX ( transpiled before it reaches the JS ) - PARCEL - BABEL
+// JSX => Babel transpiles it to Reaact.createElement => ReactElement-JS Obj => HTMLElement(render)
+//React Element
+const Title = () => (
+    <h1 className="head" tabIndex="5">Hello World from JSX</h1>
+);
+//React Component
+const HeadingComponent = () =>(
+     <div id="container">
+     {<Title/>}
+     <h1 className="heading">This is React functional component</h1>
+     </div>   
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
