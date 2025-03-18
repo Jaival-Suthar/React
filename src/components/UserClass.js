957e3613f5be78b03a5c8fd7733438df3f38b794
Jaival <jaivalsuthar21@gmail.com>
2025-03-18 16:15:59 +0530

    feat: Add state variable `count` in functional and class-based components
    - Added `count` state in `User.js` (functional component) using `useState`
    - Added `count` state in `UserClass.js` (class-based component) using `this.state`

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,14 +5,21 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         console.log(props);
+
+        this.state={
+            count:0,
+            count2:2
+        };
     };
 
     render(){
         //Let's destructure it so it looks good. And we dont't have to use this.props again
         const {name, location, status}=this.props;
-        
+        const { count, count2  }=this.state;
         return (
             <div className="user-card">
+                <h1>Count: {count}</h1>
+                <h1>Count: {count2}</h1>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Status: {status}</h4>
