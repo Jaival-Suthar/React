27c0ff7a8a6b8c86d640ac754988721e5bb23ab1
Jaival Suthar <jaivalsuthar21@gmail.com>
2025-09-28 17:08:32 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -58,3 +58,77 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RouterProvider router={appRouter} />
 );
+
+/*
+import React, {lazy, Suspense} from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Body from "./components/Body";
+//import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+//import Grocery from "./components/Grocery";
+
+import "./index.css";
+// Import PrimeReact CSS
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { ProgressSpinner } from 'primereact/progressspinner';
+
+const Grocery = lazy(()=>import("./components/Grocery"));
+const About = lazy(()=>import("./components/About"));
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Outlet />
+        </div>
+    )
+}
+
+const LoadingSpinner = () => (
+    <div className="flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+        <ProgressSpinner />
+    </div>
+);
+
+const appRouter= createBrowserRouter([
+    {//Takes list of paths
+        path:"/",
+        element:<AppLayout />,
+        children:[
+            {
+                path:"/",
+                element:<Body />
+            },
+            {
+                path:"/about",
+                element:<About />
+            },
+            {
+                path:"/contact",
+                element:<Contact />
+            },
+            {
+                path:"/grocery",
+                element:<Grocery />
+            },
+            {
+                path:"/restaurants/:resId",
+                element:<RestaurantMenu />
+            },
+        ],
+        errorElement:<Error />
+    },
+    
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Suspense fallback={<LoadingSpinner />}>
+  <RouterProvider router={appRouter} />
+</Suspense>
+);
+*/
